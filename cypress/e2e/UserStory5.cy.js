d3248a307ecf507d8b5fa405fa4aaed61da96aa4
AdamWahlstrm <wahlstrmadam@gmail.com>
2023-06-02 15:06:02 +0200

    UserStory5-klar

--- cypress/e2e/UserStory5.cy.js
+++ cypress/e2e/UserStory5.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Booking', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+
+    cy.get('.navigation .navigation__icon').click();
+
+    it('det ska gå att klicka på navicon', () => {
+      cy.get('.navigation__link').should('have.length', 2);
+    });
+
+    it('Correct Path', () => {
+      cy.get('.navigation__link').last().click();
+      cy.location('pathname').should('include', '/confirmation');
+    });
+
+    it('Correct Path', () => {
+      cy.get('.navigation__link').first().click();
+      cy.location('pathname').should('include', '/#');
+    });
+  });
+});
