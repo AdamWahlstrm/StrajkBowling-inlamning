e78cff9e0cba992f3860889d8958c5277cf20e22
AdamWahlstrm <wahlstrmadam@gmail.com>
2023-06-02 14:12:42 +0200

    UserStory1-klar

--- cypress/e2e/UserStory1.cy.js
+++ cypress/e2e/UserStory1.cy.js
@@ -1,6 +1,27 @@
-describe('Besök bokningssidan', () => {
-  it('Öppnar bokningssidan', () => {
-    cy.visit('http://localhost:5173/#'); // Byt ut URL:en om det behövs
-    cy.get('.booking-info__heading').should('contain', 'When, WHAT & Who');
+describe('bookingPage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/#');
+  });
+
+  it('add date for booking', () => {
+    cy.get('.input__field').first().should('be.visible');
+  });
+
+  it('add time for booking', () => {
+    cy.get('.input__field').eq(1).should('be.visible');
+    cy.get('.input__field').eq(1).type('16');
+    cy.get('.input__field').eq(1).should('have.value', '16');
+  });
+
+  it('add number of people playing', () => {
+    cy.get('.input__field').eq(2).should('be.visible');
+    cy.get('.input__field').eq(2).type('2');
+    cy.get('.input__field').eq(2).should('have.value', '2');
+  });
+
+  it('add number of lanes ', () => {
+    cy.get('.input__field').eq(3).should('be.visible');
+    cy.get('.input__field').eq(3).type('4');
+    cy.get('.input__field').eq(3).should('have.value', '4');
   });
 });
