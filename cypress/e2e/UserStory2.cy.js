f2fadf19fb6cefcc66af1076b00b63dec684c832
AdamWahlstrm <wahlstrmadam@gmail.com>
2023-06-02 14:22:35 +0200

    UserStory2-klar

--- cypress/e2e/UserStory2.cy.js
+++ cypress/e2e/UserStory2.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Booking', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('Should be possible to add one pair of shoes', () => {
+    cy.get('.shoes__button').click();
+    cy.get('.shoes__form').should('be.visible');
+    cy.get('.input__field.shoes__input').type('43').should('have.value', '43');
+  });
+
+  it('Should be possible to add more than one shoe', () => {
+    cy.get('.shoes__button').click();
+    cy.get('.shoes__form').should('be.visible');
+    cy.get('.input__field.shoes__input').type('43').should('have.value', '43');
+    cy.get('.shoes__button').eq(1).click();
+    cy.get('.input__field.shoes__input')
+      .eq(1)
+      .type('41')
+      .should('have.value', '41');
+  });
+});
