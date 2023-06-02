e3f61754b09faf2843e7a593ad815ea7f63616a7
AdamWahlstrm <wahlstrmadam@gmail.com>
2023-06-02 14:30:24 +0200

    UserStory3-klar

--- cypress/e2e/UserStory3.cy.js
+++ cypress/e2e/UserStory3.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Booking', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+
+    cy.get('.shoes__button').as('addButton').click().click();
+
+    cy.get('.shoes__input').eq(0).type('41');
+
+    cy.get('.shoes__input').eq(1).type('44');
+
+    cy.get('.shoes__button--small').last().click();
+
+    cy.get('.shoes_input').should('have.length', 1);
+  });
+});
